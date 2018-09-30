676c9bf6ea85a672017761cbf985977ca08e4b7d
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-09-30 13:48:07 +0300

    Variables

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
-namespace Test_1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Test_1 {
+    class Program {
+        static void Main (string[] args) {
+            string characterName = "Mike";
+            int characterAge = 18;
+            Console.WriteLine ("There once was a man named " + characterName);
+            Console.WriteLine ("And he was " + (characterAge + 8) + " years old");
+
+            characterName = "John";
+            Console.WriteLine (" ");
+            Console.WriteLine ("He really like the name " + characterName);
+            Console.WriteLine ("But didn't like been " + characterAge);
         }
     }
 }
